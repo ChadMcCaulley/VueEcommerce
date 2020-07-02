3daaf38e70a9ba587a7b9a576b6ab32e7a3a1da7
Chad McCaulley <chad.mccaulley@pgm-corp.com>
2020-07-01 22:14:32 -0700

    Installed and added vee-validate to Vue globally

--- src/main.js
+++ src/main.js
@@ -3,9 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+import { ValidationProvider, ValidationObserver } from 'vee-validate'
 import './registerServiceWorker'
+import './validation'
 
 Vue.config.productionTip = false
+
+Vue.component('ValidationProvider', ValidationProvider)
+Vue.component('ValidationObserver', ValidationObserver)
 
 new Vue({
   router,
