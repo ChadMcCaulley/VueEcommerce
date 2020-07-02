e1a9737507a06d3d369e0a4cd2b1ad261e2380af
Chad McCaulley <chad.mccaulley@pgm-corp.com>
2020-07-01 17:57:53 -0700

    All requests now go through the api localhost:8000 in development

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,15 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
+  devServer: {
+    proxy: {
+      '^/': {
+        target: 'localhost:8000',
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  },
   chainWebpack: config => {
     config
       .plugin('html')
