66ed6749079e728936f38f60b745514b5ee89516
Chad McCaulley <chad.mccaulley@pgm-corp.com>
2020-07-18 16:29:03 -0700

    Added a dedicated password input field

---
+++ src/components/PasswordInput.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-text-field
+    v-bind="$props"
+    :label="label || 'Password'"
+    :counter="counter || '32'"
+    :type="displayType"
+    :append-icon="displayIcon"
+    @click:append="toggleIcon"
+    @input="$emit('input', $event)"
+  />
+</template>
+
+<script>
+import { VTextField } from 'vuetify/lib'
+
+export default {
+  name: 'PasswordInput',
+  extends: VTextField,
+  data: () => ({
+    displayType: 'password',
+    displayIcon: 'mdi-eye'
+  }),
+  methods: {
+    /**
+     * Change the icon and text display type of the password input
+     */
+    toggleIcon () {
+      this.displayType = this.displayType === 'text' ? 'password' : 'text'
+      this.displayIcon = this.displayIcon === 'mdi-eye' ? 'mdi-eye-off' : 'mdi-eye'
+    }
+  }
+}
+</script>
