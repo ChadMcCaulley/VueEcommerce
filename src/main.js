dbe9cee93cf810a16961a5a4e6073920776ceb8f
Chad McCaulley <chad.mccaulley@pgm-corp.com>
2020-07-18 21:09:29 -0700

    Added vue-cookies config

--- src/main.js
+++ src/main.js
@@ -3,21 +3,30 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
-import Autocomplete from '@/components/Autocomplete'
-import axios from 'axios'
 import { ValidationProvider, ValidationObserver } from 'vee-validate'
+import axios from 'axios'
+import VueCookies from 'vue-cookies'
+import Autocomplete from '@/components/Autocomplete'
+import ItemCard from '@/components/ItemCard'
+import PasswordInput from '@/components/PasswordInput'
+import mixins from '@/mixins'
 import '@/registerServiceWorker'
 import '@/plugins/vee-validate'
-import mixins from '@/mixins'
 
 axios.defaults.baseURL = process.env.VUE_APP_AXIOS_BASE_URL || ''
 
 Vue.config.productionTip = false
 
 Vue.component('Autocomplete', Autocomplete)
+Vue.component('ItemCard', ItemCard)
+Vue.component('PasswordInput', PasswordInput)
 Vue.component('ValidationProvider', ValidationProvider)
 Vue.component('ValidationObserver', ValidationObserver)
+
 Vue.mixin(mixins)
+
+Vue.use(VueCookies)
+Vue.$cookies.config('30d')
 
 new Vue({
   router,
