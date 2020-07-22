ad58c8ece030cef25c1625a932949402bc384661
Chad McCaulley <chad.mccaulley@pgm-corp.com>
2020-07-21 23:08:21 -0700

    Api responses should now be caching correctly. New content will be displayed without user prompt )

--- vue.config.js
+++ vue.config.js
@@ -20,5 +20,25 @@ module.exports = {
         args[0].title = 'Ecom'
         return args
       })
+  },
+  pwa: {
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+      navigateFallback: '/index.html',
+      runtimeCaching: [
+        {
+          urlPattern: new RegExp('^https://portfolio-ecom.herokuapp.com/'),
+          handler: 'networkFirst',
+          options: {
+            networkTimeoutSeconds: 20,
+            cacheName: 'api-cache',
+            cacheableResponse: {
+              statuses: [0, 200]
+            }
+          }
+        }
+      ],
+      skipWaiting: true
+    }
   }
 }
